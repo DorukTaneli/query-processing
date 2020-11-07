0fd9325dee48e4c881ad05a999aac35227cf4c12
Radovan1997 <rvitek41@gmail.com>
2020-11-07 15:00:50 +0100

    initialization of Sort Merge

--- Source/ShapeCount.c
+++ Source/ShapeCount.c
@@ -1,7 +1,26 @@
 typedef void* SortMergeJoinDatabase;
-SortMergeJoinDatabase SortMergeJoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
-void SortMergeJoinInsertEdge(SortMergeJoinDatabase database, int fromNodeID, int toNodeID,
-                             int edgeLabel);
+SortMergeJoinDatabase SortMergeJoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd) {
+    int db[totalNumberOfEdgesInTheEnd][3];
+    memset(db, -1, sizeof(db[0][0]) * totalNumberOfEdgesInTheEnd * 3);
+    //USE -1 to denote a free spot in the db
+
+}
+
+void SortMergeJoinInsertEdge(SortMergeJoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel) {
+    struct Database* db = ((struct Database*)database);
+
+    int insert[3] = {fromNodeID, toNodeID, edgeLabel};
+
+    for (int i = 0; i<(sizeof(db)/sizeof(db)); i++ ) {
+        if (db[i][0] == -1) {
+
+        }
+
+    }
+
+
+}
+
 int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int edgeLabel2,
                           int edgeLabel3);
 void SortMergeJoinDeleteEdge(SortMergeJoinDatabase database, int fromNodeID, int toNodeID,
@@ -24,3 +43,7 @@ int CompetitionRunQuery(CompetitionDatabase database, int edgeLabel1, int edgeLa
 void CompetitionDeleteEdge(CompetitionDatabase database, int fromNodeID, int toNodeID,
                            int edgeLabel);
 void CompetitionDeleteDatabase(CompetitionDatabase database);
+
+
+//OUR HELPER CODE BELOW
+
