d2493f72de5e807f6b48b6ca7e3c2ebdeb3c9259
Radovan1997 <rvitek41@gmail.com>
2020-11-08 17:34:13 +0000

    print debug

--- Source/ShapeCount.c
+++ Source/ShapeCount.c
@@ -45,7 +45,7 @@ void SortMergeJoinInsertEdge(SortMergeJoinDatabase database, int fromNodeID, int
     struct edge *db = (struct edge *)database;
 
     int totNoEdges = sizeof(db)/sizeof(struct edge);
-    printf("Total Edges: %d \n", totNoEdges);
+    printf("Total Edges: %d \n", sizeof(db));
 
     for (int i = 0; i<totNoEdges; i++) {
         printf("%d\n", db[i].edgeLabel);
