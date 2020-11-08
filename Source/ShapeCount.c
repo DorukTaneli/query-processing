e4c6572b84e7a7cbaec2fac6fcdd8877913d88c7
Doruk Taneli <doruk.taneli20@imperial.ac.uk>
2020-11-08 13:26:55 +0100

    undefined reference fix

--- Source/ShapeCount.c
+++ Source/ShapeCount.c
@@ -190,11 +190,21 @@ void SortMergeJoinDeleteDatabase(SortMergeJoinDatabase database) {
 }
 
 typedef void* HashjoinDatabase;
-HashjoinDatabase HashjoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
-void HashjoinInsertEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel);
-int HashjoinRunQuery(HashjoinDatabase database, int edgeLabel1, int edgeLabel2, int edgeLabel3);
-void HashjoinDeleteEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel);
-void HashjoinDeleteDatabase(HashjoinDatabase database);
+HashjoinDatabase HashjoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd){
+
+}
+void HashjoinInsertEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel){
+
+}
+int HashjoinRunQuery(HashjoinDatabase database, int edgeLabel1, int edgeLabel2, int edgeLabel3){
+    return 5;
+}
+void HashjoinDeleteEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel){
+
+}
+void HashjoinDeleteDatabase(HashjoinDatabase database){
+
+}
 
 typedef void* CompetitionDatabase;
 CompetitionDatabase CompetitionAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
