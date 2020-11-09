01fdb9e6a0b27279a713038614de5d6a05260aa5
Doruk Taneli <doruk.taneli20@imperial.ac.uk>
2020-11-09 17:26:55 +0100

    typo

--- Source/ShapeCount.c
+++ Source/ShapeCount.c
@@ -110,7 +110,7 @@ int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int ed
         }
     }
 
-    for(int i=0; i< sizeof(edge1matches)/sizeof(edge1matches[0]; i++)){
+    for(int i=0; i< sizeof(edge1matches)/sizeof(edge1matches[0]); i++){
         printf("For: %d, To: %d, Label: %d", edge1matches[i].fromNode, edge1matches[i].toNode, edge1matches[i].edgeLabel);
     }
 
