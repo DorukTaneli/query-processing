4d8b2855af6a80f14a87827667b6ba28ac52f58c
Doruk Taneli <doruk.taneli20@imperial.ac.uk>
2020-11-16 13:47:47 +0100

    make hashdb competitiondb

--- Source/ShapeCount.c
+++ Source/ShapeCount.c
@@ -128,7 +128,7 @@ int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int ed
         }
     }
 
-
+/*
     printf("Edge1matches: \n ");
     for(int i=0; i< sizeof(edge1matches)/sizeof(edge1matches[0]); i++){
         if (edge1matches[i].edgeLabel != -1)
@@ -146,7 +146,7 @@ int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int ed
         if (edge3matches[i].edgeLabel != -1)
             printf("For: %d, To: %d, Label: %d \n", edge3matches[i].fromNode, edge3matches[i].toNode, edge3matches[i].edgeLabel);
     }
-
+*/
 
     //printf("Arrays filled \n");
 
@@ -323,7 +323,7 @@ int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int ed
     int final_count = 0;
 
     
-
+/*
     printf("VALIDS 1: \n");
     for(int i=0; i< sizeof(valids1)/sizeof(valids1[0]); i++){
         if (valids1[i].edgeLabel != -1){
@@ -344,7 +344,7 @@ int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int ed
             printf("For: %d, To: %d, Label: %d \n", valids3_FINAL[i].fromNode, valids3_FINAL[i].toNode, valids3_FINAL[i].edgeLabel);
         }
     }
-
+*/
     
 
 
@@ -1017,14 +1017,383 @@ void HashjoinDeleteDatabase(HashjoinDatabase database){
 }
 
 typedef void* CompetitionDatabase;
-CompetitionDatabase CompetitionAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
-int CompetitionFindEdge(CompetitionDatabase database, int fromNodeID, int toNodeID, int edgeLabel);
-void CompetitionInsertEdge(CompetitionDatabase database, int fromNodeID, int toNodeID,
-                           int edgeLabel);
-int CompetitionRunQuery(CompetitionDatabase database, int edgeLabel1, int edgeLabel2,
-                        int edgeLabel3);
-void CompetitionDeleteEdge(CompetitionDatabase database, int fromNodeID, int toNodeID,
-                           int edgeLabel);
-void CompetitionDeleteDatabase(CompetitionDatabase database);
+
+CompetitionDatabase CompetitionAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd){
+    struct edge_db *dbstruct = (struct edge_db *) malloc(sizeof(*dbstruct) + (sizeof(struct edge) * (totalNumberOfEdgesInTheEnd*hashattribute)));
+    
+    struct edge *db = dbstruct->db;
+
+    dbstruct->length = totalNumberOfEdgesInTheEnd*hashattribute;
+
+    for (int i = 0; i < totalNumberOfEdgesInTheEnd*hashattribute; i++) {
+        db[i].fromNode = -1;
+        db[i].toNode = -1;
+        db[i].edgeLabel = -1;
+    }
+
+    return (void*) dbstruct;
+}
+
+int CompetitionFindEdge(SortMergeJoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel) {
+    struct edge_db *dbstruct = (struct edge_db *) database;
+    struct edge *db = dbstruct->db;
+    int hashTableSize = dbstruct->length;
+
+    struct edge probeInput = {fromNodeID, toNodeID, edgeLabel};
+    int hashValue = myHash(probeInput, hashTableSize);
+
+    int quad = 1;
+    while (1) {
+        if (db[(hashValue + quad)%hashTableSize].fromNode == probeInput.fromNode &&
+            db[(hashValue + quad)%hashTableSize].toNode == probeInput.toNode &&
+            db[(hashValue + quad)%hashTableSize].edgeLabel == probeInput.edgeLabel) {
+                return probeInput.edgeLabel;  
+        }
+
+        if (quad > hashTableSize) {
+            return -1;
+        }
+
+        quad++;
+    }
+}
+
+void CompetitionInsertEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel){
+    //printf("InsertEdge called\n");
+    struct edge_db *dbstruct = (struct edge_db *) database;
+
+    struct edge *db = dbstruct->db;
+
+    int hashTableSize = dbstruct->length;
+
+    struct edge buildInput = {fromNodeID, toNodeID, edgeLabel};
+    int hashValue = myHash(buildInput, hashTableSize);
+
+    int quad = 1;
+    while (1) {
+        if (db[(hashValue + quad)%hashTableSize].edgeLabel == -1) {
+            db[(hashValue + quad)%hashTableSize] = buildInput;
+            //printf("Inserted edge: %d \n", db[i].edgeLabel);
+            break;    
+        }
+        quad++;
+    }
+}
+
+int CompetitionRunQuery(HashjoinDatabase database, int edgeLabel1, int edgeLabel2, int edgeLabel3){
+    //printf("_____FUNCTION START_______ \n");
+    struct edge_db *dbstruct = (struct edge_db *) database;
+    struct edge *db = dbstruct->db;
+    int hashTableSize = dbstruct->length;
+    //printf("Run query called \n");
+
+    struct edge edge1matches[hashTableSize]; //TO
+    struct edge edge2matches[hashTableSize]; //FROM
+    struct edge edge3matches[hashTableSize]; //FROM
+
+    //printf("Edge match arrays init complete\n");
+
+    for (int i = 0; i<hashTableSize; i++) {
+        edge1matches[i].fromNode = -1;
+        edge1matches[i].toNode = -1;
+        edge1matches[i].edgeLabel = -1;
+
+        edge2matches[i].fromNode = -1;
+        edge2matches[i].toNode = -1;
+        edge2matches[i].edgeLabel = -1;
+
+        edge3matches[i].fromNode = -1;
+        edge3matches[i].toNode = -1;
+        edge3matches[i].edgeLabel = -1;
+    }
+
+
+    //edge1 - to
+    for (int i = 0; i<hashTableSize; i++) {
+        if (db[i].edgeLabel == edgeLabel1) {
+            int hashValue = toHash(edgeLabel1, hashTableSize);
+
+            int quad = 1;
+            while (1) {
+                if (edge1matches[(hashValue + quad)%hashTableSize].edgeLabel == -1) {
+                    edge1matches[(hashValue + quad)%hashTableSize].edgeLabel = edgeLabel1;
+                    edge1matches[(hashValue + quad)%hashTableSize].toNode = db[i].toNode;
+                    edge1matches[(hashValue + quad)%hashTableSize].fromNode = db[i].fromNode;
+                    //printf("Inserted edge: %d \n", db[i].edgeLabel);
+                    break;    
+                }
+                quad++;
+            }
+        }
+    }
+
+    //edge2 - from
+    for (int i = 0; i<hashTableSize; i++) {
+        if (db[i].edgeLabel == edgeLabel2) {
+            int hashValue = fromHash(edgeLabel2, hashTableSize);
+
+            int quad = 1;
+            while (1) {
+                if (edge2matches[(hashValue + quad)%hashTableSize].edgeLabel == -1) {
+                    edge2matches[(hashValue + quad)%hashTableSize].edgeLabel = edgeLabel2;
+                    edge2matches[(hashValue + quad)%hashTableSize].toNode = db[i].toNode;
+                    edge2matches[(hashValue + quad)%hashTableSize].fromNode = db[i].fromNode;
+                    //printf("Inserted edge: %d \n", db[i].edgeLabel);
+                    break;    
+                }
+                quad++;
+            }
+        }
+    }
+
+    //edge3 - from
+    for (int i = 0; i<hashTableSize; i++) {
+        if (db[i].edgeLabel == edgeLabel3) {
+            int hashValue = fromHash(edgeLabel3, hashTableSize);
+
+            int quad = 1;
+            while (1) {
+                if (edge3matches[(hashValue + quad)%hashTableSize].edgeLabel == -1) {
+                    edge3matches[(hashValue + quad)%hashTableSize].edgeLabel = edgeLabel3;
+                    edge3matches[(hashValue + quad)%hashTableSize].toNode = db[i].toNode;
+                    edge3matches[(hashValue + quad)%hashTableSize].fromNode = db[i].fromNode;
+                    //printf("Inserted edge: %d \n", db[i].edgeLabel);
+                    break;    
+                }
+                quad++;
+            }
+        }
+    }
+
+
+    // edges1 toNode = edges2 fromNode
+    struct edge valids1[hashTableSize]; //FROM
+    struct edge valids2[hashTableSize]; //TO
+    struct edge valids3[hashTableSize]; 
+
+    for(int i=0; i<hashTableSize; i++) {
+        valids1[i].fromNode = -1;
+        valids1[i].toNode = -1;
+        valids1[i].edgeLabel = -1;
+
+        valids2[i].fromNode = -1;
+        valids2[i].toNode = -1;
+        valids2[i].edgeLabel = -1;
+
+        valids3[i].fromNode = -1;
+        valids3[i].toNode = -1;
+        valids3[i].edgeLabel = -1;      
+    }
+
+    //ITERATING OVER EDGE 1 MATCHES
+    for (int iter = 0; iter<hashTableSize; iter++){
+
+        int fromHashEdge2 = fromHash(edge1matches[iter].toNode, hashTableSize);
+        int linear_proberE2M = 0;
+        int already_inserted = 0;
+
+        while (linear_proberE2M<hashTableSize){
+            if(edge2matches[(fromHashEdge2 + linear_proberE2M)%hashTableSize].fromNode == edge1matches[iter].toNode) {
+                //INSERT INTO VALIDS 1 AND VALIDS 2
+                //valids 1 insert
+                int valids1_toHash = toHash(edge1matches[iter].toNode, hashTableSize);
+                int valids1_linear_prober = 0;
+                while (valids1_linear_prober<hashTableSize){
+                    //IF EMPTY SPOT FOUND INSERT
+                    if (valids1[(valids1_toHash+valids1_linear_prober)%hashTableSize].edgeLabel == -1 && (already_inserted == 0)){
+                        valids1[(valids1_toHash+valids1_linear_prober)%hashTableSize].fromNode = edge1matches[iter].fromNode;
+                        valids1[(valids1_toHash+valids1_linear_prober)%hashTableSize].toNode = edge1matches[iter].toNode;
+                        valids1[(valids1_toHash+valids1_linear_prober)%hashTableSize].edgeLabel = edge1matches[iter].edgeLabel;
+                        already_inserted = 1;
+                        break;
+                    }
+                    valids1_linear_prober++;
+                }
+
+                //valids 2 insert
+                int valids2_fromHash = fromHash(edge1matches[iter].fromNode, hashTableSize);
+                int valids2_linear_prober = 0;
+
+                while (valids2_linear_prober<hashTableSize){
+                    //IF EMPTY SPOT FOUND INSERT
+                    if (valids2[(valids2_fromHash+valids2_linear_prober)%hashTableSize].edgeLabel == -1){
+                        valids2[(valids2_fromHash+valids2_linear_prober)%hashTableSize].fromNode = edge2matches[(fromHashEdge2 + linear_proberE2M)%hashTableSize].fromNode;
+                        valids2[(valids2_fromHash+valids2_linear_prober)%hashTableSize].toNode = edge2matches[(fromHashEdge2 + linear_proberE2M)%hashTableSize].toNode;
+                        valids2[(valids2_fromHash+valids2_linear_prober)%hashTableSize].edgeLabel = edge2matches[(fromHashEdge2 + linear_proberE2M)%hashTableSize].edgeLabel;
+                        break;
+                    }
+                    valids2_linear_prober++;
+                }
+
+                //break;
+            }
+        linear_proberE2M++;
+        }
+    }
+
+    //TESTING 
+
+    /*
+
+    printf("Edge1: \n");
+    for (int i = 0; i<hashTableSize; i++){
+        if (edge1matches[i].edgeLabel != -1) {
+            printf("fromNode: %d, toNode: %d, edgeLabel %d \n", edge1matches[i].fromNode,edge1matches[i].toNode, edge1matches[i].edgeLabel);
+        }
+    }
+
+    printf("Edge2: \n");
+    for (int i = 0; i<hashTableSize; i++){
+        if (edge2matches[i].edgeLabel != -1) {
+            printf("fromNode: %d, toNode: %d, edgeLabel %d \n", edge2matches[i].fromNode,edge2matches[i].toNode, edge2matches[i].edgeLabel);
+        }
+    }
+
+    printf("Edge3: \n");
+    for (int i = 0; i<hashTableSize; i++){
+        if (edge3matches[i].edgeLabel != -1) {
+            printf("fromNode: %d, toNode: %d, edgeLabel %d \n", edge3matches[i].fromNode,edge3matches[i].toNode, edge3matches[i].edgeLabel);
+        }
+    }
+
+    printf("VALIDS 1: \n");
+    for (int i = 0; i<hashTableSize; i++){
+        if (valids1[i].edgeLabel != -1) {
+            printf("fromNode: %d, toNode: %d, edgeLabel %d \n", valids1[i].fromNode,valids1[i].toNode, valids1[i].edgeLabel);
+        }
+    }
+    printf("VALIDS 2: \n");
+    for (int i = 0; i<hashTableSize; i++){
+        if (valids2[i].edgeLabel != -1) {
+            printf("fromNode: %d, toNode: %d, edgeLabel %d \n", valids2[i].fromNode,valids2[i].toNode, valids2[i].edgeLabel);
+        }
+    }
+
+    */
+
+    //for loop through edge3matches then see if the edge corresponds to a valid in both valids1 and valids2
+
+
+    for (int i = 0; i<hashTableSize;i++){
+        int valids1_edge3toHash = toHash(edge3matches[i].toNode, hashTableSize);
+        int valids1_edge3_lp = 0;
+        int valids2_edge3fromHash = fromHash(edge3matches[i].fromNode, hashTableSize); 
+        int valids2_edge3_lp = 0;
+        int isMatch1 = 0;
+        int isMatch2 = 0;
+
+        while (valids1_edge3_lp<hashTableSize) {
+            if(valids1[(valids1_edge3toHash+valids1_edge3_lp)%hashTableSize].fromNode == edge3matches[i].toNode){
+                if (edge3matches[i].toNode != -1){
+                    isMatch1 = 1;
+                }
+            }
+            valids1_edge3_lp++;
+        }
+
+        while (valids2_edge3_lp<hashTableSize) {
+            if(valids2[(valids2_edge3fromHash+valids2_edge3_lp)%hashTableSize].toNode == edge3matches[i].fromNode){
+                if (edge3matches[i].toNode != -1){
+                    isMatch2 = 1;
+                }
+            }
+            valids2_edge3_lp++;
+        }
+
+        if ((isMatch1 == 1) && (isMatch2 == 1)){
+            int valids3_toHash = toHash(edge3matches[i].toNode, hashTableSize);
+            int valids3_lp = 0;
+
+            while (valids3_lp < hashTableSize){
+                if(valids3[(valids3_toHash+valids3_lp)%hashTableSize].fromNode == -1){
+                    valids3[(valids3_toHash+valids3_lp)%hashTableSize].fromNode = edge3matches[i].fromNode;
+                    valids3[(valids3_toHash+valids3_lp)%hashTableSize].toNode = edge3matches[i].toNode;
+                    valids3[(valids3_toHash+valids3_lp)%hashTableSize].edgeLabel = edge3matches[i].edgeLabel;
+                    break;
+                }
+                valids3_lp++;
+            }
+        }
+    }
+    /*
+    printf("VALIDS 3: \n");
+    for (int i = 0; i<hashTableSize; i++){
+        if (valids3[i].edgeLabel != -1) {
+            printf("fromNode: %d, toNode: %d, edgeLabel %d \n", valids3[i].fromNode,valids3[i].toNode, valids3[i].edgeLabel);
+        }
+    }
+    */
+
+    //Run through valids3
+    //If match on valids1 has a triangle with 
+
+    int valids1_counter = 0;
+    int valids2_counter = 0;
+    int valids3_counter = 0;
+
+    for (int i = 0; i<hashTableSize; i++){
+        if (valids3[i].edgeLabel != -1) {
+            valids3_counter++;
+        }
+    }
+
+    for (int i = 0; i<hashTableSize; i++){
+        if (valids2[i].edgeLabel != -1) {
+            valids2_counter++;
+        }
+    }
+    for (int i = 0; i<hashTableSize; i++){
+        if (valids1[i].edgeLabel != -1) {
+            valids1_counter++;
+        }
+    }
+
+    int final_counts[3] = {valids1_counter, valids2_counter, valids3_counter};
+    int final_count = valids1_counter;
+
+    for (int i = 0; i<3;i++){
+        if (final_counts[i] >final_count){
+            final_count = final_counts[i];
+        }
+        if (final_counts[i] == 0){
+            return 0;
+        }
+    }
+
+    return final_count;
+}
+
+void CompetitionDeleteEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel){
+    struct edge_db *dbstruct = (struct edge_db *) database;
+    struct edge *db = dbstruct->db;
+    int hashTableSize = dbstruct->length;
+
+    struct edge probeInput = {fromNodeID, toNodeID, edgeLabel};
+    int hashValue = myHash(probeInput, hashTableSize);
+
+    int quad = 1;
+    while (1) {
+        if (db[(hashValue + quad)%hashTableSize].fromNode == probeInput.fromNode &&
+            db[(hashValue + quad)%hashTableSize].toNode == probeInput.toNode &&
+            db[(hashValue + quad)%hashTableSize].edgeLabel == probeInput.edgeLabel) {
+                db[(hashValue + quad)%hashTableSize].fromNode = -1;
+                db[(hashValue + quad)%hashTableSize].toNode = -1;
+                db[(hashValue + quad)%hashTableSize].edgeLabel = -1;
+                break;
+        }
+
+        //If edge is not in hashTable
+        if (quad > hashTableSize) {
+            break;
+        }
+
+        quad++;
+    }
+}
+
+void CompetitionDeleteDatabase(HashjoinDatabase database){
+    struct edge_db *dbstruct = (struct edge_db *) database;
+    free(dbstruct);
+}
 
 
